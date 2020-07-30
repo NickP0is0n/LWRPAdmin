9dadc12ff7a1d36164004568bec7be0d07d747b4
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-07-30 14:33:18 +0300

    Тесты для случаев с null в JSON

--- app/src/test/java/live/nickp0is0n/lwrpadmin/DataReceiverTest.kt
+++ app/src/test/java/live/nickp0is0n/lwrpadmin/DataReceiverTest.kt
@@ -14,6 +14,7 @@ import live.nickp0is0n.lwrpadmin.network.StatsReceiver
 import live.nickp0is0n.lwrpadmin.network.UserCredentialsReceiver
 import org.json.JSONObject
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class DataReceiverTest {
@@ -28,7 +29,7 @@ class DataReceiverTest {
     }
 
     @Test
-    fun getData_retrieveSampleAdminInfoFromJson_userObjectReturned() {
+    fun getData_retrieveSampleAdminInfoFromJson_adminObjectReturned() {
         val sampleAdminInJson = "{\"nickname\": \"John_Smith\",\n" +
                 "\"adminLevel\": \"12\",\n" +
                 "\"mondayOnline\": \"1\",\n" +
@@ -47,5 +48,35 @@ class DataReceiverTest {
         val rightAdmin = Admin("John_Smith", 12, 1, 2, 3, 4, 5, 6, 7, 100, 200, 300, 1)
         val testAdmin = receiver.getData()
         assertEquals(rightAdmin, testAdmin)
+    }
+
+    @Test
+    fun getData_retrieveSampleUserInfoWithNullUsernameFromJson_nullReturned() {
+        val sampleUserInJson = "{\"nickname\": null, \"password\":\"qwerty123\"}"
+        val receiver = UserCredentialsReceiver()
+        receiver.receiveData(JSONObject(sampleUserInJson))
+        val testUser = receiver.getData()
+        assertNull(testUser)
+    }
+
+    @Test
+    fun getData_retrieveSampleAdminInfoWithNullUsernameFromJson_nullReturned() {
+        val sampleAdminInJson = "{\"nickname\": \"null\",\n" +
+                "\"adminLevel\": \"12\",\n" +
+                "\"mondayOnline\": \"1\",\n" +
+                "\"tuesdayOnline\": \"2\",\n" +
+                "\"wednesdayOnline\": \"3\",\n" +
+                "\"thursdayOnline\": \"4\",\n" +
+                "\"fridayOnline\": \"5\",\n" +
+                "\"saturdayOnline\": \"6\",\n" +
+                "\"sundayOnline\": \"7\",\n" +
+                "\"reportsAnswered\": \"100\",\n" +
+                "\"muted\": \"200\",\n" +
+                "\"jailed\": \"300\",\n" +
+                "\"pAvig\": \"1\"}"
+        val receiver = StatsReceiver()
+        receiver.receiveData(JSONObject(sampleAdminInJson))
+        val testAdmin = receiver.getData()
+        assertNull(testAdmin)
     }
 }
