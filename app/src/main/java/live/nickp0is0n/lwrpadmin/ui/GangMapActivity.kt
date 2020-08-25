132c8027577143485f2ee322837d7c8ce3d0babf
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-08-25 16:53:42 +0300

    Добавлена инициализация интерфейса Gang Map

--- app/src/main/java/live/nickp0is0n/lwrpadmin/ui/GangMapActivity.kt
+++ app/src/main/java/live/nickp0is0n/lwrpadmin/ui/GangMapActivity.kt
@@ -13,12 +13,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_gang_map.*
 import live.nickp0is0n.lwrpadmin.R
+import live.nickp0is0n.lwrpadmin.models.GangMap
 
 class GangMapActivity : AppCompatActivity() {
+    private lateinit var gangMap: GangMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gang_map)
+        gangMap = intent.extras?.get("gangMap") as GangMap
+        initializeGangMapUI()
         playTopBarAnimation()
+    }
+
+    private fun initializeGangMapUI() {
+        grovePlayerCount.text = gangMap.groveOnline.toString()
+        groveTerritoryCount.text = gangMap.groveTerritories.toString()
+        ballasPlayerCount.text = gangMap.ballasOnline.toString()
+        ballasTerritoryCount.text = gangMap.ballasTerritories.toString()
+        vagosPlayerCount.text = gangMap.vagosOnline.toString()
+        vagosTerritoryCount.text = gangMap.vagosTerritories.toString()
+        rifaPlayerCount.text = gangMap.rifaOnline.toString()
+        rifaTerritoryCount.text = gangMap.rifaTerritories.toString()
+        aztecPlayerCount.text = gangMap.aztecOnline.toString()
+        aztecTerritoryCount.text = gangMap.aztecTerritories.toString()
+        lastUpdateDate.text = gangMap.lastUpdate
     }
 
     private fun playTopBarAnimation() {
