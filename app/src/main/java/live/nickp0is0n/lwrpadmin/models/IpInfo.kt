be17fa54046c69bb2e79eb73fbbd7154095a9567
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-09-23 21:58:53 +0300

    Добавлен код для получения информации по IP

---
+++ app/src/main/java/live/nickp0is0n/lwrpadmin/models/IpInfo.kt
@@ -0,0 +1,27 @@
+/*
+ * Это - исходный код приложения LWRP Admin v2.x.
+ * Данное приложение является пропиетарным ПО и
+ * его код не подлежит распространению без согласия автора.
+ *
+ * Copyright Mykola Chaikovskyi, 2020.
+ */
+
+package live.nickp0is0n.lwrpadmin.models
+
+import io.ipinfo.api.IPInfo
+
+data class IpInfo (val ip: String) {
+    val provider: String
+    val country: String
+    val city: String
+
+    val ACCESS_TOKEN = "7aa3f036a9508e"
+
+    init {
+        val ipInfoProvider = IPInfo.builder().setToken(ACCESS_TOKEN).build()
+        val ipInfoResponse = ipInfoProvider.lookupIP(ip)
+        provider = ipInfoResponse.company.name
+        country = ipInfoResponse.countryCode
+        city = ipInfoResponse.city
+    }
+}
