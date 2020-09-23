5cfadea3359c9a6c2d26f32b42819b18ae8aa6e6
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-09-23 22:38:06 +0300

    Добавлена разметка для activity IP информации

---
+++ app/src/main/java/live/nickp0is0n/lwrpadmin/ui/IpInfoActivity.kt
@@ -0,0 +1,25 @@
+/*
+ * Это - исходный код приложения LWRP Admin v2.x.
+ * Данное приложение является пропиетарным ПО и
+ * его код не подлежит распространению без согласия автора.
+ *
+ * Copyright Mykola Chaikovskyi, 2020.
+ */
+
+package live.nickp0is0n.lwrpadmin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import live.nickp0is0n.lwrpadmin.R
+
+class IpInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ip_info)
+    }
+
+    fun onRetrieveInfoButtonClick(view: View) {
+
+    }
+}
